82814c6933223f0db29cb237b1331a1624efdb2f
Danhee Kim <127651317+Dani-K1@users.noreply.github.com>
2023-09-09 19:16:39 +0900

    1주차 수업 - 과제?

---
+++ 230908-1주차 수업.c
@@ -0,0 +1,38 @@
+//HELLO WORLD
+
+#include <stdio.h>
+
+//main function
+// int main(void){
+
+//     printf("HELLO WORLD.\n"); //헬로 월드 출력
+//     printf("HELLO KOREA.\n"); //헬로 코리아 출력
+
+//     printf("%d\n", 1234);
+//     printf("%d %d\n", 10, 20);
+
+//     printf("My age: %d\n", 20);
+//     printf("%d is my point \n", 100);
+//     printf("Good morning everybody\n");
+
+//     return 0; //0의 반환
+// } // main function end
+
+//메인함수
+int main(void){
+    printf("========Code 1========\n");
+    printf("홍길동\n홍 길 동\n홍  길  동\n");
+
+    printf("========Code 2========\n");
+    printf("이름 : 김단희\n");
+    printf("주소 : 서울시 구로구 나머진 비밀\n");
+    printf("전화번호 : 010-0000-0000\n");
+
+    printf("========Homeworkd0========\n");
+    printf("C프로그래밍1 1주차 과제\n");
+    printf("이름 : 김단희\n");
+    int i = 202115057;
+    printf("학번 : %d\n", i);
+
+    return 0;
+}
